600d6c1f5a6e0419ae22815a33f7cd43271ae6c6
DaffaFauzyN <daffafauzynugraha@gmail.com>
2026-05-26 22:04:55 +0700

    remove debug banner

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -1,11 +1,10 @@
 import Script from "next/script";
 
 export default function GoogleAnalytics({ gaId }) {
-  if (!gaId) return <div id="ga-debug" style={{display:"block",background:"red",color:"white",padding:"10px",textAlign:"center"}}>GA DISABLED (no gaId)</div>;
+  if (!gaId) return null;
 
   return (
     <>
-      <div id="ga-debug" style={{display:"block",background:"green",color:"white",padding:"10px",textAlign:"center"}}>GA ACTIVE: {gaId}</div>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
         strategy="afterInteractive"
