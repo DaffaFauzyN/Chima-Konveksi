1480bd03f1dcab3b02fc81ca1d2a4c1a6c962847
DaffaFauzyN <daffafauzynugraha@gmail.com>
2026-05-30 12:25:58 +0700

    proxy.js default export untuk redirect Vercel domain & non-www

---
+++ proxy.js
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+
+export default function proxy(request) {
+  const host = request.headers.get("host") || "";
+  const { pathname, search } = request.nextUrl;
+
+  if (host.includes("vercel.app")) {
+    const url = new URL(pathname, "https://www.chimakonveksi.my.id");
+    url.search = search;
+    return NextResponse.redirect(url, 301);
+  }
+
+  if (host === "chimakonveksi.my.id") {
+    const url = new URL(pathname, "https://www.chimakonveksi.my.id");
+    url.search = search;
+    return NextResponse.redirect(url, 301);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+};
